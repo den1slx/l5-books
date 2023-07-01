e61b941f09691639287cf9f509a2f919831c66bc
den1slx <keycoingold@mail.ru>
2023-07-01 15:41:18 +1000

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,7 +1,29 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 import json
 import argparse
+
+
+def on_reload():
+    parser = create_parser()
+    args = parser.parse_args()
+    path = args.path
+    with open(path, 'r', encoding='UTF-8') as f:
+        books = json.load(f)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        books=books,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 
 def create_parser():
@@ -13,27 +35,11 @@ def create_parser():
 
 
 def main():
-    parser = create_parser()
-    args = parser.parse_args()
-    path = args.path
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
-
-    with open(path, 'r', encoding='UTF-8') as f:
-        books = json.load(f)
-
-    rendered_page = template.render(
-        books=books,
-    )
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+    on_reload()  # for args.path
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+    # http://127.0.0.1:5500/
 
 
 if __name__ == '__main__':
